6f904d28fa8c18f3d3f9710325f4cea8629504ca
Gabriel Barimboim <gdbarimboim@gmail.com>
2025-03-26 16:00:16 -0300

    add cart component to navbar; update navbar template and styles

--- src/navbar/navbar.component.ts
+++ src/navbar/navbar.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { OffScreenMenuComponent } from './off-screen-menu/off-screen-menu.component';
 import { OffScreenMenuService } from '../services/off-screen-menu.service';
+import { CartComponent } from './cart/cart.component';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [OffScreenMenuComponent],
+  imports: [OffScreenMenuComponent, CartComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
