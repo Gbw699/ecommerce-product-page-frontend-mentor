d86517f93ebb3ebaa6ed30142ddfe86798251ada
Gabriel Barimboim <gdbarimboim@gmail.com>
2025-03-26 12:38:34 -0300

    implement off-screen menu in navbar; add toggle functionality and update styles

--- src/navbar/navbar.component.ts
+++ src/navbar/navbar.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, viewChild } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
+  offScreenMenu: any = viewChild('offScreenMenu');
 
+  toggleMenu() {
+    this.offScreenMenu()?.nativeElement.classList.toggle('open-menu');
+  }
 }
