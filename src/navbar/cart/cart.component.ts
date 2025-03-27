01c214dc9f6e5ab837a413fccd432ac022ea9bc5
Gabriel Barimboim <gdbarimboim@gmail.com>
2025-03-27 11:20:31 -0300

    refactor cart component; update styles, toggle functionality, and close cart on outside click

--- src/navbar/cart/cart.component.ts
+++ src/navbar/cart/cart.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, computed, HostListener, viewChild } from '@angular/core';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
   imports: [],
   templateUrl: './cart.component.html',
-  styleUrl: './cart.component.scss'
+  styleUrl: './cart.component.scss',
 })
 export class CartComponent {
+  cartFlag = computed(() => {
+    return this.cartService.cartFlag();
+  });
+  container: any = viewChild('container');
 
+  constructor(private cartService: CartService) {}
+
+  @HostListener('document:click', ['$event'])
+  closeCart(event: any) {
+    const container = this.container()?.nativeElement;
+    if (!container) return;
+
+    const { top, left, right, bottom } = container.getBoundingClientRect();
+
+    if (
+      event.clientX < left ||
+      event.clientX > right ||
+      event.clientY > bottom
+    ) {
+      this.cartService.setCartFlag(false);
+    }
+  }
 }
--- src/navbar/navbar.component.ts
+++ src/navbar/navbar.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { OffScreenMenuComponent } from './off-screen-menu/off-screen-menu.component';
 import { OffScreenMenuService } from '../services/off-screen-menu.service';
 import { CartComponent } from './cart/cart.component';
+import { CartService } from '../services/cart.service';
 
 @Component({
   selector: 'app-navbar',
@@ -11,9 +12,17 @@ import { CartComponent } from './cart/cart.component';
   styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
-  constructor(private offScreenMenuService: OffScreenMenuService) {}
+  constructor(
+    private offScreenMenuService: OffScreenMenuService,
+    private cartService: CartService
+  ) {}
 
   toggleMenu() {
     this.offScreenMenuService.setMenuFlag(true);
+    this.cartService.setCartFlag(false);
+  }
+
+  toggleCart() {
+    this.cartService.setCartFlag(!this.cartService.cartFlag());
   }
 }
